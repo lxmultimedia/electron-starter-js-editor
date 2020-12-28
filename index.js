a473b3600fc151ebfeea4d48182144cd5025a049
Attilio Furci <lxmediacom@gmail.com>
2020-12-28 09:43:48 +0100

    MainWindows Handling

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const {app, BrowserWindow} = require('electron')
 
-app.on('ready', () => {
+
+let mainWindow;
+
+function createWindow() {
   mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname+'/img/icon.png',
     webPreferences: {
-      nodeIntegration: 
-      true
-    } 
+      nodeIntegration: true
+    }
   })
   mainWindow.loadFile("renderer.html")
+
+  //win.webContents.openDevTools()
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if(process.platform!=='darwin') {
+    app.quit();
+  }
 })
